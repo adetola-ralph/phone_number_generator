ad64a725c1b653bac56145638e353af86371134e
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-01-25 21:31:46 +0100

    Retrieve minimum and maximum numbers

--- server/controllers/phone-number.controller.js
+++ server/controllers/phone-number.controller.js
@@ -7,15 +7,39 @@ module.exports = {
     var data = numberGenerator.generateNumbers(amount);
     // store the number(s) here
     phoneModel.addValue(data).then(function () {
-    res.json({
-      success: true,
-      data: data,
-    });
+      res.json({
+        success: true,
+        data: data,
+      });
     }).catch(function (){
       res.status(500).json({
         success: false,
         message: 'there was an error please try again',
       });
+    });
+  },
+
+  getAllNumbers: function (req, res) {
+    var data = phoneModel.getAll();
+    res.json({
+      success: true,
+      data: data,
+    });
+  },
+
+  getMinNumber: function (req, res) {
+    var data = phoneModel.getMin();
+    res.json({
+      success: true,
+      data: data,
+    });
+  },
+
+  getMaxNumber: function (req, res) {
+    var data = phoneModel.getMax();
+    res.json({
+      success: true,
+      data: data,
     });
   },
 
--- server/routes/phone-number.routes.js
+++ server/routes/phone-number.routes.js
@@ -2,6 +2,12 @@ var phoneNumberController = require('./../controllers/phone-number.controller');
 
 module.exports = function (router) {
   router.route('/phone-numbers')
+    .post(phoneNumberController.generateNumbers)
+    .get(phoneNumberController.getAllNumbers);
+
+  router.get('/phone-numbers/min', phoneNumberController.getMinNumber);
+
+  router.get('/phone-numbers/max', phoneNumberController.getMaxNumber);
 
   router.get('/phone-numbers/total', phoneNumberController.getTotalNumber);
 };
