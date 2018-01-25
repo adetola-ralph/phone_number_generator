31c399b9242330f7a6fa8e409a97e42404c43f6b
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-01-25 16:21:34 +0100

    Install and setup gulp for build process

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var nodemon = require('gulp-nodemon');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function () {
+  return browserify('./app/app.js')
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('copy', ['browserify'], function () {
+  return gulp.src('./app/**/*.html')
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('serve:dev', ['build'],function () {
+  nodemon({
+    script: './server/index.js',
+    env: {
+      'NODE_ENV': 'development',
+    },
+    ignore: ['public/**/*'],
+    ext: 'js,html',
+  });
+
+  console.log('Watching files for changes.');
+});
+
+gulp.task('build', ['copy']);
+
+gulp.task('default', ['serve:dev'], function () {
+  gulp.watch('./app/**/*.*', ['build']);
+});
