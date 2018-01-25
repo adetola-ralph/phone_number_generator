b5a59f3ed02eb4113457749969c9c6650a2a412d
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-01-25 21:31:15 +0100

    Store generated phone numbers

--- server/controllers/phone-number.controller.js
+++ server/controllers/phone-number.controller.js
@@ -1,13 +1,21 @@
 var numberGenerator = require('./../utils/generateRandomNumbers');
+var phoneModel = require('./../models/phone-number.model');
 
 module.exports = {
   generateNumbers: function (req, res) {
     var amount = req.body.amount || 1;
     var data = numberGenerator.generateNumbers(amount);
     // store the number(s) here
+    phoneModel.addValue(data).then(function () {
     res.json({
       success: true,
       data: data,
+    });
+    }).catch(function (){
+      res.status(500).json({
+        success: false,
+        message: 'there was an error please try again',
+      });
     });
   },
 
