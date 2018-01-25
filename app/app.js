5ee0609f69114be7ab7adc55c2e23d83e102f8fd
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-01-25 23:48:19 +0100

    Add uirouter for routing in angularjs

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 var angular = require('angular');
+var uiRouter = require('@uirouter/angularjs');
 
-var app = angular.module('RandomPhoneNumberGenerator', []);
+var app = angular.module('RandomPhoneNumberGenerator', ['ui.router']);
 
-app.controller('TestController', function () {
-  this.name = 'Olutola';
+app.config(function ($stateProvider) {
+  $stateProvider.state('stats', {
+    url: '/',
+    template: '<stats-component></stats-component>'
+  })
 })
 
 require('./components/index');
