45fcc54f489ecfcac47613f607ecb19e49f4956b
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-01-26 06:41:50 +0100

    Add default state handler for frontend routes

--- app/app.js
+++ app/app.js
@@ -3,7 +3,14 @@ var uiRouter = require('@uirouter/angularjs');
 
 var app = angular.module('RandomPhoneNumberGenerator', ['ui.router']);
 
-app.config(function ($stateProvider) {
+app.config(function ($stateProvider, $urlRouterProvider) {
+  function defaultStateHandler($state) {
+    $state.go('stats');
+  }
+  $urlRouterProvider.when('', defaultStateHandler);
+
+  $urlRouterProvider.when('/', defaultStateHandler);
+
   $stateProvider.state('stats', {
     url: '/',
     template: '<stats-component></stats-component>',
